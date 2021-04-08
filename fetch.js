683d1f5f3d386c27d8ea07f2717779e8245d78cd
FrancoPaganucci <francoepaganucci@gmail.com>
2021-04-07 22:07:04 -0300

    commit nuevo

--- fetch.js
+++ fetch.js
@@ -1,6 +1,8 @@
+
+
 function Search() {
-    async function newSearch(/*parámetro que pasa el usuario*/) {
-        const url = "";
+    async function newSearch(q) {
+        const url = "https://newsapi.org/v2/everything?q="+q"&from=2021-03-08&sortBy=publishedAt&apiKey="+api_key;
         const resp = await fetch(url);
         const info = await resp.json();
         return info;
@@ -11,7 +13,16 @@ function Search() {
     let info = newSearch(input_value);
     info.then(response => {
         //funcion que crea la noticia.
+        createNews()
     }).catch(error => {
         console.log(error);
     })
+}
+
+// funcion que crea la noticia.ç
+// tendríamos que hacer un document.get del componente al que vamos a appendear la noticia
+function createNews(x) {
+    for (let i=0;i<x.length;i++) {
+        //generar noticia dinámicamente
+    }
 }
