ad5f3129919613763219847ff5e5f18b1f3b4c68
FrancoPaganucci <francoepaganucci@gmail.com>
2021-04-07 22:27:36 -0300

    estructura de la funcion fetch y crear noticia

--- fetch.js
+++ fetch.js
@@ -13,16 +13,31 @@ function Search() {
     let info = newSearch(input_value);
     info.then(response => {
         //funcion que crea la noticia.
-        createNews()
+        noticia = createNews();
+        // al final del proceso hace append al contenedor
+        news_ctn.appendChild(noticia);
     }).catch(error => {
         console.log(error);
     })
 }
 
-// funcion que crea la noticia.ç
-// tendríamos que hacer un document.get del componente al que vamos a appendear la noticia
+// funcion que crea la noticia.
+// document.get del componente al que vamos a appendear la noticia
+news_ctn = document.getElementById("contenedor-noticias");
 function createNews(x) {
-    for (let i=0;i<x.length;i++) {
-        //generar noticia dinámicamente
+    if (/*total results === 0 */) {
+        console.error("no se encontraron noticias con esa palabra");
+    }
+    else {
+        for (let i=0;i<1;i++) {
+            //generar noticia dinámicamente
+            let title = document.getElementById("titulo");
+            title.innerHTML = /* posicion del JSON correspondiente*/
+            let photo = document.getElementById("imagen-noticia");
+            photo.className = /*clase estilo imagenes */
+            photo.setAttribute("src", /* posicion del JSON corresp*/)
+            let content = document.getElementById("texto-noticia");
+            content.innerHTML = /* posicion del JSON corresp */
+        }
     }
 }
