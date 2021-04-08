e6ee5ecce90a3c862cb8b200234d403c34c9caa8
FrancoPaganucci <francoepaganucci@gmail.com>
2021-04-07 21:52:49 -0300

    actualizo fetch.js

---
+++ fetch.js
@@ -0,0 +1,17 @@
+function Search() {
+    async function newSearch(/*parámetro que pasa el usuario*/) {
+        const url = "";
+        const resp = await fetch(url);
+        const info = await resp.json();
+        return info;
+    }
+
+    // fetch con promesa
+    let input_value = /*valor del input buscador*/
+    let info = newSearch(input_value);
+    info.then(response => {
+        //funcion que crea la noticia.
+    }).catch(error => {
+        console.log(error);
+    })
+}
